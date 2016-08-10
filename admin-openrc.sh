bf026f3b169cc73f96c6e52bd0f9383d7a1ce1d4
rdudyala <rdudyala@clnode114.clemson.cloudlab.us>
2016-08-10 02:54:27 -0400

    Added v2 version of keystone api in admin-openrc

--- admin-openrc.sh
+++ admin-openrc.sh
@@ -1,9 +1,8 @@
-export OS_PROJECT_DOMAIN_NAME=default
-export OS_USER_DOMAIN_NAME=default
+unset OS_PROJECT_DOMAIN_ID
+unset OS_USER_DOMAIN_ID
 export OS_PROJECT_NAME=admin
+export OS_TENANT_NAME=admin
 export OS_USERNAME=admin
 export OS_PASSWORD=password
-export OS_AUTH_URL=http://localhost:35357/v3
-export OS_IDENTITY_API_VERSION=3
-export OS_IMAGE_API_VERSION=2
+export OS_AUTH_URL=http://localhost:35357/v2.0
 ceilometer meter-list
